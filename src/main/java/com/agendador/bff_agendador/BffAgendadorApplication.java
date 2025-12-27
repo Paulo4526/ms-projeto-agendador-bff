a5b3c27f3d053c74c4bd613abf7473dd7d348513
Paulo4526 <paulosilvabueno@hotmail.com>
2025-12-27 10:10:12 -0300

    Primeiro commit

---
+++ src/main/java/com/agendador/bff_agendador/BffAgendadorApplication.java
@@ -0,0 +1,13 @@
+package com.agendador.bff_agendador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffAgendadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffAgendadorApplication.class, args);
+	}
+
+}
